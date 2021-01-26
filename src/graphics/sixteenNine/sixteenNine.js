f06a637d8f391682979cb03eabb3f6da65116416
Jai Sharma <jai@jai.moe>
2021-01-26 23:02:18 +0000

    Add details

--- src/graphics/sixteenNine/sixteenNine.js
+++ src/graphics/sixteenNine/sixteenNine.js
@@ -4,16 +4,57 @@ import gsap from 'gsap';
 import '../common.css'
 import './sixteenNine.css';
 
+const runRep = NodeCG.Replicant('runDataActiveRun', 'nodecg-speedcontrol');
+const timerRep = NodeCG.Replicant('timer', 'nodecg-speedcontrol');
+
+const blankRun = {
+  category: 'Nipple %',
+  estimate: '00:04:20',
+  game: 'Some Cool Game',
+  release: '1996',
+  system: 'Arch Linux',
+  teams: [
+    {
+      players: [
+        { name: 'jai_' },
+      ],
+    },
+  ],
+};
+
+const safeRun = () => (runRep.value || blankRun);
+const sep = '/';
+
 class SixteenNineComponent {
   view() {
-    return [
+    return m('.graphic', [
       m('.game'),
       m('.left', [
         m('.cam'),
       ]),
-      m('.bottom'),
-    ];
+      m('.bottom',[
+        m('.run-details', [
+          m('.run-game', safeRun().game),
+          m('.run-details-row', [
+            m('div', safeRun().system),
+            m('div', sep),
+            m('div', safeRun().release),
+            m('div', sep),
+            m('div', safeRun().category),
+          ]),
+        ]),
+        m('.run-timing', [
+          m('.timer', timerRep.value.time),
+          m('.estimate', `Estimate: ${safeRun().estimate}`),
+        ]),
+      ]),
+    ]);
   }
 }
 
-m.mount(document.body, SixteenNineComponent);
+NodeCG.waitForReplicants(runRep, timerRep).then(() => {
+  m.mount(document.body, SixteenNineComponent);
+});
+
+runRep.on('change', () => { m.redraw(); });
+timerRep.on('change', () => { m.redraw(); });
