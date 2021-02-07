1db08798cd933b0d6f5f2210384d18c02c802c17
Jai Sharma <jai@jai.moe>
2021-02-07 20:20:02 +0000

    Add countdown to break screen and fix z-index issues

--- src/graphics/break/break.js
+++ src/graphics/break/break.js
@@ -9,6 +9,7 @@ import CurrentSongComponent from '../currentSong/currentSong.js';
 const runRep = NodeCG.Replicant('runDataActiveRun', 'nodecg-speedcontrol');
 const totalRep = NodeCG.Replicant('total', 'nodecg-tiltify');
 const currentSongRep = NodeCG.Replicant('currentSong', 'nodecg-spotify');
+const countdownRep = NodeCG.Replicant('countdown', 'wasd2021');
 
 class BreakComponent {
   view(vnode) {
@@ -18,7 +19,7 @@ class BreakComponent {
         m('.left', [
           m('.countdown-container', [
             m('.countdown-label', 'BACK SOON'),
-            m('.countdown-time', '00:00:00'),
+            m('.countdown-time', vnode.attrs.countdown.display),
           ]),
           m('.logo .wasd'),
           m('.logo .special-effect'),
@@ -31,12 +32,13 @@ class BreakComponent {
   }
 }
 
-NodeCG.waitForReplicants(runRep, totalRep, currentSongRep).then(() => {
+NodeCG.waitForReplicants(runRep, totalRep, currentSongRep, countdownRep).then(() => {
   m.mount(document.body, {
     view: () => {
       return m(BreakComponent, {
         currentSong: currentSongRep.value,
         total: Math.floor(totalRep.value),
+        countdown: countdownRep.value,
       });
     }
   });
@@ -46,3 +48,4 @@ NodeCG.waitForReplicants(runRep, totalRep, currentSongRep).then(() => {
 runRep.on('change', () => { m.redraw(); });
 totalRep.on('change', () => { m.redraw(); });
 currentSongRep.on('change', () => { m.redraw(); });
+countdownRep.on('change', () => { m.redraw(); });
