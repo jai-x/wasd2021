042d6ba47acc076564dbc6b64b4fda71b3734ac1
Jai Sharma <jai@jai.moe>
2021-02-12 22:52:06 +0000

    Add donation incentives to break screen

--- src/graphics/break/break.js
+++ src/graphics/break/break.js
@@ -13,11 +13,54 @@ import { nextRuns, NextRunsComponent } from '../nextRuns/nextRuns.js';
 const replicants = {
   run: NodeCG.Replicant('runDataActiveRun', 'nodecg-speedcontrol'),
   runArray: NodeCG.Replicant('runDataArray', 'nodecg-speedcontrol'),
-  total: NodeCG.Replicant('total', 'nodecg-tiltify'),
   currentSong: NodeCG.Replicant('currentSong', 'nodecg-spotify'),
   countdown: NodeCG.Replicant('countdown', 'wasd2021'),
   currentVideo: NodeCG.Replicant('currentVideo', 'wasd2021'),
+  total: NodeCG.Replicant('total', 'nodecg-tiltify'),
+  challenges: NodeCG.Replicant('challenges', 'nodecg-tiltify'),
 };
+
+class Incentive {
+  view(vnode) {
+    const { name, amount, totalAmountRaised } = vnode.attrs.incentive;
+    const [ gameName, incentiveName ] = name.split('-').map(s => s.trim());
+
+    return m('.incentive-container', [
+      m('.incentive-game', gameName),
+      m('.incentive-name', incentiveName),
+      m('.incentive-bar', [
+        m('.incentive-progress'),
+        m('.incentive-amount', `£${totalAmountRaised} / £${amount}`),
+      ]),
+    ]);
+  }
+
+  onupdate(vnode) {
+    const bar = vnode.dom.children[2].children[0];
+
+    const current = Number(vnode.attrs.incentive.totalAmountRaised);
+    const max = Number(vnode.attrs.incentive.amount);
+
+    const width = Math.min(((current / max) * 100), 100);
+
+    gsap.to(bar, {
+      width: `${width}%`,
+      ease: 'expo.out',
+      duration: 3,
+    });
+  }
+}
+
+class Incentives {
+  view(vnode) {
+    return m('.incentives-container', [
+      m('.incentives-label', 'DONATION INCENTIVES'),
+      m('.incentives-list', ...vnode.attrs.incentives.filter(i => i.active).map((i) => {
+        return m(Incentive, { incentive: i, key: i.id });
+      })),
+    ]);
+  }
+}
 
 class BreakComponent {
   view(vnode) {
@@ -46,9 +89,7 @@ class BreakComponent {
               m('.total-amount', `£${vnode.attrs.total}`),
             ]),
             m('.h-space'),
-            m('.incentives-container', [
-              m('.incentives-label', 'DONATION INCENTIVES'),
-            ]),
+            m(Incentives, { incentives: vnode.attrs.incentives }),
           ]),
           m(VideoPlayer, { currentVideo: vnode.attrs.currentVideo }),
         ]),
@@ -67,6 +108,7 @@ NodeCG.waitForReplicants(...Object.values(replicants)).then(() => {
         countdown: replicants.countdown.value,
         currentVideo: replicants.currentVideo.value,
         nextRuns: nextRuns(replicants.run.value, replicants.runArray.value),
+        incentives: replicants.challenges.value,
       });
     }
   });
